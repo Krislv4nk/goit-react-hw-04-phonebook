fa8395118f86c9ec6b3331f4cb761bf16ba1910a
Krislv4nk <136389095+Krislv4nk@users.noreply.github.com>
2024-01-06 20:45:58 +0100

    useState, useEffect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 
 import css from './App.module.css';
@@ -7,35 +7,26 @@ import { ContactForm } from './contactForm/contactForm';
 import { Filter } from './filter/filter';
 import { ContactList } from './contactList/contactList';
 
-export  class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+export const App = () => {
+  
+  const [filter, setFilter] = useState('');
+  const [contacts, setContacts] = useState([{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-    addedToLocalSt: false,
-  };
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },]);
 
-componentDidUpdate(prevProps, prevState) {
-  if (prevState.contacts !== this.state.contacts){
-    window.localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-  }
-}
-
-componentDidMount() {
+useEffect(() => {
   const contacts = window.localStorage.getItem('contacts');
-  
   if (contacts) {
-    this.setState({ contacts: JSON.parse(contacts) });
+    setContacts(JSON.parse(contacts));
   }
-}
+}, []);
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
   
 
-  handleAddedContact = formData => {
-    const { contacts } = this.state;
+  const handleAddedContact = formData => {
 
     const hasDuplicates = contacts.some(
       ({ name, number }) =>
@@ -51,41 +42,113 @@ componentDidMount() {
       ...formData,
       id: nanoid(5),
     };
-    this.setState(({ contacts }) => {
-      return { contacts: [...contacts, newContact] };
-    });
+    setContacts(prevContacts => [...prevContacts, newContact]);
   };
 
-  handleFilterChange = e => {
-    const filterValue = e.currentTarget.value;
-    this.setState({ filter: filterValue });
+  const handleFilterChange = e => {
+    setFilter(e.target.value);
   };
 
-  handleDeleteBtnClick = contactId => {
-    this.setState(({ contacts }) => {
-      return {
-        contacts: contacts.filter(({ id }) => id !== contactId),
-      };
-    });
+  const handleDeleteBtnClick = contactId => {
+    setContacts(prevContacts => prevContacts.filter(({ id }) => id !== contactId));
   };
-  render() {
-    const { contacts, filter } = this.state;
     const filteredContacts = contacts.filter(({ name }) =>
       name.toLowerCase().includes(filter.trim().toLowerCase())
-    );
+  );
 
     return (
       <div className={css.container}>
         <h1 className={css.phoneBookTitle}>Phonebook</h1>
-        <ContactForm onSubmit={this.handleAddedContact} />
+        <ContactForm onSubmit={handleAddedContact} />
         <h2 className={css.contactTitle}>Contacts</h2>
-        <Filter value={filter} onChange={this.handleFilterChange} />
+        <Filter value={filter} onChange={handleFilterChange} />
         <ContactList
           contacts={filteredContacts}
-          handleDeleteBtnClick={this.handleDeleteBtnClick}
+          handleDeleteBtnClick={handleDeleteBtnClick}
         />
       </div>
     );
   }
-}
 
+
+// export  class App extends Component {
+//   state = {
+//     contacts: [
+//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+//     ],
+//     filter: '',
+//     addedToLocalSt: false,
+//   };
+
+// componentDidUpdate(prevProps, prevState) {
+//   if (prevState.contacts !== this.state.contacts){
+//     window.localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+//   }
+// }
+
+// componentDidMount() {
+//   const contacts = window.localStorage.getItem('contacts');
+  
+//   if (contacts) {
+//     this.setState({ contacts: JSON.parse(contacts) });
+//   }
+// }
+  
+
+//   handleAddedContact = formData => {
+//     const { contacts } = this.state;
+
+//     const hasDuplicates = contacts.some(
+//       ({ name, number }) =>
+//         name.toLowerCase() === formData.name.toLowerCase() || 
+//         number === formData.number
+//     );
+//     if (hasDuplicates) {
+//       alert(`Contact ${formData.name} is already in contacts.`);
+//       return;
+//     }
+
+//     const newContact = {
+//       ...formData,
+//       id: nanoid(5),
+//     };
+//     this.setState(({ contacts }) => {
+//       return { contacts: [...contacts, newContact] };
+//     });
+//   };
+
+//   handleFilterChange = e => {
+//     const filterValue = e.currentTarget.value;
+//     this.setState({ filter: filterValue });
+//   };
+
+//   handleDeleteBtnClick = contactId => {
+//     this.setState(({ contacts }) => {
+//       return {
+//         contacts: contacts.filter(({ id }) => id !== contactId),
+//       };
+//     });
+//   };
+//   render() {
+//     const { contacts, filter } = this.state;
+//     const filteredContacts = contacts.filter(({ name }) =>
+//       name.toLowerCase().includes(filter.trim().toLowerCase())
+//     );
+
+//     return (
+//       <div className={css.container}>
+//         <h1 className={css.phoneBookTitle}>Phonebook</h1>
+//         <ContactForm onSubmit={this.handleAddedContact} />
+//         <h2 className={css.contactTitle}>Contacts</h2>
+//         <Filter value={filter} onChange={this.handleFilterChange} />
+//         <ContactList
+//           contacts={filteredContacts}
+//           handleDeleteBtnClick={this.handleDeleteBtnClick}
+//         />
+//       </div>
+//     );
+//   }
+// }
--- src/components/contactForm/contactForm.jsx
+++ src/components/contactForm/contactForm.jsx
@@ -1,49 +1,106 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import css from "./contactForm.module.css";
 
-export class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
 
-  handleFormSubmit = (evt) => {
+export const ContactForm = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleFormSubmit = (evt) => {
     evt.preventDefault();
 
-    const name = evt.currentTarget.elements.name.value;
-    const number = evt.currentTarget.elements.number.value;
-
-    this.setState({ name: name, number: number });
-    this.props.onSubmit({ name, number });
-    evt.currentTarget.reset();
+    onSubmit({ name, number });
+    setName('');
+    setNumber('');
   };
-  render() {
+
+  const handleNameChange = (evt) => {
+    setName(evt.target.value);
+  };
+
+  const handleNumberChange = (evt) => {
+    setNumber(evt.target.value);
+  };
+ 
+
+  
     return (
-      <form className={css.contactForm} onSubmit={this.handleFormSubmit}>
-        <label className={css.label}>
-          Name
-          <input
-            className={css.contactInput}
-            type="text"
-            name="name"
-            placeholder="Enter name"
-            required
-          />
-        </label>
-        <label className={css.label}>
-          Number
-          <input
-            className={css.contactInput}
-            type="tel"
-            name="number"
-            placeholder="Enter phone number"
-            required
-          />
-        </label>
-        <button className={css.addContactBttn} type="submit">
-          Add contact
-        </button>
-      </form>
-    );
+      <form className={css.contactForm} onSubmit={handleFormSubmit}>
+      <label className={css.label}>
+        Name
+        <input
+          className={css.contactInput}
+          type="text"
+          name="name"
+          placeholder="Enter name"
+          required
+          value={name}
+          onChange={handleNameChange}
+        />
+      </label>
+      <label className={css.label}>
+        Number
+        <input
+          className={css.contactInput}
+          type="tel"
+          name="number"
+          placeholder="Enter phone number"
+          required
+          value={number}
+          onChange={handleNumberChange}
+        />
+      </label>
+      <button className={css.addContactBttn} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
   }
-}
+
+
+// export class ContactForm extends Component {
+//   state = {
+//     name: '',
+//     number: '',
+//   };
+
+//   handleFormSubmit = (evt) => {
+//     evt.preventDefault();
+
+//     const name = evt.currentTarget.elements.name.value;
+//     const number = evt.currentTarget.elements.number.value;
+
+//     this.setState({ name: name, number: number });
+//     this.props.onSubmit({ name, number });
+//     evt.currentTarget.reset();
+//   };
+//   render() {
+//     return (
+//       <form className={css.contactForm} onSubmit={this.handleFormSubmit}>
+//         <label className={css.label}>
+//           Name
+//           <input
+//             className={css.contactInput}
+//             type="text"
+//             name="name"
+//             placeholder="Enter name"
+//             required
+//           />
+//         </label>
+//         <label className={css.label}>
+//           Number
+//           <input
+//             className={css.contactInput}
+//             type="tel"
+//             name="number"
+//             placeholder="Enter phone number"
+//             required
+//           />
+//         </label>
+//         <button className={css.addContactBttn} type="submit">
+//           Add contact
+//         </button>
+//       </form>
+//     );
+//   }
+// }
